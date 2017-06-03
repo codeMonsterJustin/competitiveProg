ec7e15c2e2c453610c243b0cd855410f720a3390
Justin Dang <justin.ht.dang@gmail.com>
2017-06-04 01:20:19 +1000

    Add soln for Beholder.java

---
+++ ICPCprep/ANZAC2017Beginner/Beholder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Beholder{
+     public static void main(String[] args){
+
+       Scanner scan = new Scanner(System.in);
+
+       long n = scan.nextLong();
+       long x = scan.nextLong();
+
+       if(n == 1 && x == 0){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+        
+    }
+}
